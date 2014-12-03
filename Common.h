12bb63a49179528ee61c012e361b252a83353e74
wrbeasl <wrbeasl@g.clemson.edu>
2014-12-03 14:35:59 -0500

    Header file

---
+++ Common.h
@@ -0,0 +1,16 @@
+/*
+ * Header File with appropriate includes
+*/
+
+#include <cstdlib>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+#include <sys/stat.h>
