8fe4587f1883360dd599123348215374967a17a5
wrbeasl <wrbeasl@g.clemson.edu>
2014-12-03 15:16:56 -0500

    Just updating code

--- Client.c
+++ Client.c
@@ -3,11 +3,31 @@
 /* Args: ./client <UDP Hostname> <UDP Port> */
 int main(int argc, char **argv){
 
+	int sock, port;
+	char *Servername;
 
-
+	struct sockaddr_in serveraddr;
 
 	if(argc != 3){
-		perror("Error: Client Syntax: ./Client <UDP Hostname> <UDP Port>")
+		perror("Error: Client Syntax: ./Client <UDP Hostname> <UDP Port>\n");
+		exit(0);
 	}
+
+	port = atoi(argv[2]);
+	Servername = argv[1];
+
+	if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
+		perror("Error creating socket, please panic.\n");
+		exit(1);
+	}
+
+
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = inet_addr(Servername);
+	serveraddr.sin_port = port;
+
+
+
 	return 0;
 }
--- Common.h
+++ Common.h
@@ -16,3 +16,26 @@
 #include <sys/stat.h>
 
 #define N 6
+
+class http {
+   private:
+      char *server_name;
+   	  char *robotID;
+      uint16_t port;
+      char *dir;	   // The server directory 
+      FILE *page;  	   // The file that the request is asking for
+      char *file;	   // The file name
+      char *method;    // The method called by the client (GET | HEAD)
+      char *ext;
+      int  file_size;
+      
+      int sock;
+      int connfd;
+      struct sockaddr_in servaddr;
+
+   public:
+      http();
+      http(int port, char *servername, char *robotID);
+      ~http();
+
+};
