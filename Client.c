12d922cb70a85e55fa684d2f997da8bf3557c157
Chris Ragan <cragan@clemson.edu>
2014-12-06 13:27:49 -0500

    basic client functionality

--- Client.c
+++ Client.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv){
 	char *Servername;
 
 	struct sockaddr_in serveraddr;
-
+	robot_cmd command;
 	if(argc != 3){
 		perror("Error: Client Syntax: ./Client <UDP Hostname> <UDP Port>\n");
 		exit(0);
@@ -28,6 +28,32 @@ int main(int argc, char **argv){
 	serveraddr.sin_port = port;
 
 
+	int validcommand = 0;
+	int cmdcode;
+	int cmdvalue = 0;
+
+	while(!validcommand) {
+		printf("Command? ");
+		scanf("%d", &cmdcode);
+
+		if(cmdcode > 0 && cmdcode < 7)
+		{
+			if(cmdcode == 4)
+			{
+				printf("\nSleep how long? ");
+				scanf("%d",&cmdvalue);
+			}
+		validcommand = 1;
+		}
+	}
+
+	command.command = cmdcode;
+	command.value = cmdvalue;
+
+	sendto(sock, &command, sizeof(command), 0,
+	(struct sockaddr *) &serveraddr, sizeof(serveraddr));
+
+	
 
 	return 0;
 }
