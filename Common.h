309940d1f48152dc551488b9c20e7301a3502779
William Boatman <wboatma@clemson.edu>
2014-12-06 13:02:55 -0500

    Changed the class to a struct

--- Common.h
+++ Common.h
@@ -17,25 +17,11 @@
 
 #define N 6
 
-class http {
-   private:
-      char *server_name;
-   	  char *robotID;
-      uint16_t port;
-      char *dir;	   // The server directory 
-      FILE *page;  	   // The file that the request is asking for
-      char *file;	   // The file name
-      char *method;    // The method called by the client (GET | HEAD)
-      char *ext;
-      int  file_size;
-      
-      int sock;
-      int connfd;
-      struct sockaddr_in servaddr;
-
-   public:
-      http();
-      http(int port, char *servername, char *robotID);
-      ~http();
-
-};
+typedef struct http_struct {
+   char *server_name;
+   char *robotID;
+   uint16_t port;
+   int sock;
+   int connfd;
+   struct sockaddr_in servaddr;
+} http;
